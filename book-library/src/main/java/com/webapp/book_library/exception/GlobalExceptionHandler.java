cc0d093964e3a9c0a2698a0b75376d72cc691849
gkatsarova <g_katsarova2007@abv.bg>
2026-04-15 11:18:34 +0300

    remove stack trace exposure from REST error responses

--- book-library/src/main/java/com/webapp/book_library/exception/GlobalExceptionHandler.java
+++ book-library/src/main/java/com/webapp/book_library/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.webapp.book_library.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -7,27 +9,21 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.io.StringWriter;
-import java.io.PrintWriter;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Map<String, Object>> handleException(Exception e) {
-        System.err.println("GLOBAL EXCEPTION CAUGHT: " + e.getMessage());
-        e.printStackTrace();
-        
+        logger.error("Unhandled exception: {}", e.getMessage(), e);
+
         Map<String, Object> body = new HashMap<>();
         body.put("status", 500);
         body.put("error", "Internal Server Error");
-        body.put("message", e.getMessage());
-        body.put("type", e.getClass().getName());
-        
-        StringWriter sw = new StringWriter();
-        e.printStackTrace(new PrintWriter(sw));
-        body.put("stackTrace", sw.toString());
-        
+        body.put("message", "An unexpected error occurred. Please try again later.");
+
         return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
